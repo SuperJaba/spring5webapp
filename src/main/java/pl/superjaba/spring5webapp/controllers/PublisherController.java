66b0ecedb927e8edac2973941493201ef483ecbd
Karambix\Karambii <pichow@wp.pl>
2018-10-06 19:56:44 +0200

    new web view Publisher and all similar logic to rest

---
+++ src/main/java/pl/superjaba/spring5webapp/controllers/PublisherController.java
@@ -0,0 +1,22 @@
+package pl.superjaba.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.superjaba.spring5webapp.repositories.PublisherRepository;
+
+@Controller
+public class PublisherController {
+
+    private PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    @RequestMapping("/publishers")
+    public String getPublishers(Model model) {
+        model.addAttribute("publishers", publisherRepository.findAll());
+        return "publishers";
+    }
+}
